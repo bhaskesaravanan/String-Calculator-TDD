b6461fffba9021c33ebfd6ecc556034dfe757a4a
bhaskesaravanan <bhaskar.saravanan@anywhere.co>
2024-09-08 14:57:00 +0530

    fix: handle the add number function for the single number, multiple numbers

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 
 
 def add_numbers(number_string):
-    # return none on empty string
+    # return 0 on the empty string
     if not number_string:
         return 0
+    number_list = number_string.split(",")
+    return sum([int(number) for number in number_list])
+
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,15 @@ import pytest
 from src.main import add_numbers
 
 
-def test_add_numbers():
-    result = add_numbers("1")
+def test_empty_string():
+    result = add_numbers("")
     assert result is 0
+
+def test_single_number():
+    result = add_numbers("5")
+    assert result is 5
+
+def test_add_number():
+    result = add_numbers("5,5,5")
+    assert result is 15
+    
