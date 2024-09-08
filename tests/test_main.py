4c98476ffa76b3a8069b941cd53805d0ea0c3224
bhaskesaravanan <bhaskar.saravanan@anywhere.co>
2024-09-08 14:52:14 +0530

    feat: make the test case fail of handling the string value

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,6 @@ import pytest
 from src.main import add_numbers
 
 
-def test_print_hi():
-    result = add_numbers("")
+def test_add_numbers():
+    result = add_numbers("1")
     assert result is 0
