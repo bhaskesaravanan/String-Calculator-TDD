42c8988e0b60376da63d0641f3f4905be08c874e
bhaskesaravanan <bhaskar.saravanan@anywhere.co>
2024-09-08 14:49:46 +0530

    feat: first failed test case for the string calculator

---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+
+def add_numbers(number_string):
+    # return none on empty string
+    if not number_string:
+        return 0
+
+
+if __name__ == "__main__":
+    input_string = input("Enter a string of numbers: ")
+    print(add_numbers(input_string))
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+import pytest
+from src.main import add_numbers
+
+
+def test_print_hi():
+    result = add_numbers("")
+    assert result is 0
